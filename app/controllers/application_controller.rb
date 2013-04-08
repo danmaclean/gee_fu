076a94091d70879f9a7bdd83e779d676fb186968
Andy Shipman <andy@cllearview.com>
2013-04-08 13:56:55 +0100

    Update for Rails 3.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
+  protect_from_forgery
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  helper :all # include all helpers, all the time
+  
   def respond(response)
     respond_to do |format|
       format.html
