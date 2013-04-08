64b9a1f9dc4f42b75466e57e495de2919e439ecd
Andy Shipman <andy@cllearview.com>
2013-04-08 15:16:17 +0100

    Add whitelist attributes

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -1,6 +1,8 @@
 class Reference < ActiveRecord::Base
   has_many :features
   has_one :sequence, :dependent => :destroy
+
+  attr_accessible :name, :length
   
   #a holder method for the REST interface, allows use of reference.ref_seq attribute when GET parameter sequence=true and thereby returns the sequence as part of the same object..
   attr_accessor :ref_seq
