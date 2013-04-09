d7a29f207ccec26d1cea099352347fa1dce4e609
Andy Shipman <andy@cllearview.com>
2013-04-09 11:48:40 +0100

    Add initial security feature

---
+++ features/step_definitions/security_steps.rb
@@ -0,0 +1,39 @@
+Given(/^I am not logged in$/) do
+  # dummy step
+end
+
+Given(/^there are no users$/) do
+  # for now a dummy step
+  # User.count.should eq 0
+  # Admin.count.should eq 0
+end
+
+Then(/^I should be able to sign up$/) do
+  click_link "Sign up"
+end
+
+When(/^I sign up as "(.*?)" with email "(.*?)"$/) do |name, email|
+  full_name = name.split(" ")
+  fill_in "First name", :with => full_name[0]
+  fill_in "Last name", :with => full_name[1]
+  fill_in "Email address", :with => email
+  fill_in "Password", :with => "password"
+  fill_in "Confirm your password", :with => "password"
+  click_button "Sign up"
+end
+
+Then(/^"(.*?)" should receive an email$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I open the email$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow "(.*?)" in the email$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^"(.*?)" should be a confirmed user$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
