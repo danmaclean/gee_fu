1b3e1f9a8f9981bab8efd45981b23df08dadb4e6
Andy Shipman <andy@cllearview.com>
2013-04-09 12:36:54 +0100

    Add validation for first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :last_name, :first_name
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
