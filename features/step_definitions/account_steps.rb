560c74a762ed0b06284f1434b1842d2ddddde481
Andy Shipman <andy@cllearview.com>
2013-04-10 10:33:14 +0100

    Add affiliation/role editing

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -7,16 +7,18 @@ Given(/^there are no users$/) do
 end
 
 Then(/^I should be able to sign up$/) do
-  click_link "Sign up"
+  ->{find "a", text: "Sign up"}.should_not raise_exception
 end
 
 When(/^I sign up as "(.*?)" with email "(.*?)"$/) do |name, email|
-  full_name = name.split(" ")
-  fill_in "First name", :with => full_name[0]
-  fill_in "Last name", :with => full_name[1]
-  fill_in "Email address", :with => email
-  fill_in "Password", :with => "password"
-  fill_in "Password confirmation", :with => "password"
+  complete_sign_up_form_with(name, email)
+  click_button "Sign up"
+end
+
+When(/^I sign up as "(.*?)" with place of work "(.*?)" and role "(.*?)" and email "(.*?)"$/) do |name, place_of_work, role, email|
+  complete_sign_up_form_with(name, email)
+  fill_in "Affiliation / Place of work", :with => place_of_work
+  fill_in "Role", :with => role
   click_button "Sign up"
 end
 
@@ -39,11 +41,57 @@ end
 
 Given(/^"(.*?)" is logged in$/) do |email|
   visit "/users/sign_in"
+  set_current_user_email(email)
   fill_in "Email address", with: email
   fill_in "Password", with: "password"
   click_button "Sign in"
 end
 
 Then(/^I should be able to sign out$/) do
-  click_link "Sign Out"
+  ->{find "a", text: "Sign Out"}.should_not raise_exception
+end
+
+When(/^I edit my name to "(.*?)"$/) do |name|
+  full_name = name.split(" ")
+  fill_in "First name", :with => full_name[0]
+  fill_in "Last name", :with => full_name[1]
+  fill_in "Current password", :with => "password"
+  click_button "Update"
+end
+
+Then(/^my name should be "(.*?)"$/) do |name|
+  full_name = name.split(" ")
+  current_user.first_name.should eq full_name[0]
+  current_user.last_name.should eq full_name[1]
+end
+
+When(/^"(.*?)" confirms the account$/) do |email|
+  step %Q{"#{email}" should receive an email}
+  step %Q{I open the email}
+  step %Q{I click the first link in the email}
+end
+
+Then(/^I should have a place of work "(.*?)" and role "(.*?)"$/) do |place_of_work, role|
+  current_user.affiliation.should eq place_of_work
+  current_user.role.should eq role
+end
+
+When(/^I edit my place of work to "(.*?)"$/) do |place_of_work|
+  fill_in "Affiliation / Place of work", :with => place_of_work
+  fill_in "Current password", :with => "password"
+  click_button("Update")
+end
+
+Then(/^my place of work should be "(.*?)"$/) do |place_of_work|
+  current_user.affiliation.should eq place_of_work
+end
+
+When(/^I edit my role to "(.*?)"$/) do |role|
+  fill_in "Role", :with => role
+  fill_in "Current password", :with => "password"
+  click_button("Update")
+end
+
+Then(/^my role should be "(.*?)"$/) do |role|
+  current_user.role.should eq role
 end
