21e18ac38e4fe4a2114ac07feb7196cb3477be42
Andy Shipman <andy@cllearview.com>
2013-04-10 16:47:10 +0100

    Add feature for testing BAM

--- features/step_definitions/experiment_steps.rb
+++ features/step_definitions/experiment_steps.rb
@@ -7,7 +7,7 @@ When(/^I am ready to enter experiment data$/) do
   click_link("New experiment")
 end
 
-When(/^I add an experiment called "(.*?)", described as "(.*?)" with GFF gile "(.*?)" and "(.*?)" as the genome build$/) do |name, description, gff_file, genome_build|
+When(/^I add an experiment called "(.*?)", described as "(.*?)" with GFF file "(.*?)" and "(.*?)" as the genome build$/) do |name, description, gff_file, genome_build|
   within "div#gff_experiment_data" do
     fill_in("name (required)", :with => name)
     fill_in("description (required)", :with => description)
@@ -23,4 +23,14 @@ end
 
 When(/^I am ready to enter experiment data directly$/) do
   visit "/experiments"
+end
+
+When(/^I add an experiment called "(.*?)", described as "(.*?)" with BAM file "(.*?)" and "(.*?)" as the genome build$/) do |name, description, bam_file, genome_build|
+  within "div#bam_experiment_data" do
+    fill_in("name (required)", :with => name)
+    fill_in("description (required)", :with => description)
+    attach_file("BAM file of features (required)", "#{Rails.root}/#{bam_file}")
+    choose(genome_build)
+    click_button "Create"
+  end
 end
