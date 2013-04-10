f29c72a13935ffab6b1154c54d83f0e482a6a095
Andy Shipman <andy@cllearview.com>
2013-04-10 08:24:46 +0100

    Test we get the correct content for a signed in user

--- features/step_definitions/security_steps.rb
+++ features/step_definitions/security_steps.rb
@@ -23,3 +23,27 @@ end
 Then(/^"(.*?)" should be a confirmed user$/) do |email|
   User.where(:email => email).first.should be_confirmed
 end
+
+Given(/^there is a user called "(.*?)" with email "(.*?)"$/) do |name, email|
+  full_name = name.split(" ")
+  user = User.new(
+    email:                  email,
+    password:               "password",
+    password_confirmation:  "password",
+    first_name:             full_name[0],
+    last_name:              full_name[1]
+  )
+  user.confirm!
+  user.save!
+end
+
+Given(/^"(.*?)" is logged in$/) do |email|
+  visit "/users/sign_in"
+  fill_in "Email address", with: email
+  fill_in "Password", with: "password"
+  click_button "Sign in"
+end
+
+Then(/^I should be able to sign out$/) do
+  click_link "Sign Out"
+end
