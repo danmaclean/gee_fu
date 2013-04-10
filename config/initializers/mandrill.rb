f13b9beb97af075be2121c786bbcbaa53ab48025
Andy Shipman <andy@cllearview.com>
2013-04-10 08:23:43 +0100

    Only use Mandrill in dev/production

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,8 +1,10 @@
-ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :authentication => :plain
-}
-ActionMailer::Base.delivery_method = :smtp
+if %w(development production).include?(Rails.env)
+  ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+      :address =>        'smtp.mandrillapp.com',
+      :user_name =>      ENV['MANDRILL_USERNAME'],
+      :password =>       ENV['MANDRILL_APIKEY'],
+      :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
