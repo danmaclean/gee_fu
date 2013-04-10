bc93bbc931224bc54f183c0a540139b8a0c3c414
Andy Shipman <andy@cllearview.com>
2013-04-10 10:57:48 +0100

    Authenticate organisms, genomes, experiments

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,24 @@ GeeFu::Application.routes.draw do
 
   root :to => "pages#index"
   
-  get "/browse",    :to => "pages#browse" 
-  get "/signed_up", :to => "pages#signed_up", :as => :signed_up
+  get "/browse",     :to => "pages#browse" 
+  get "/signed_up",  :to => "pages#signed_up", :as => :signed_up
+  get "/signed_out", :to => "pages#signed_out", :as => :signed_up
+  match 'begin' => 'organisms#index'
 
-  resources :organisms
+  authenticate :user do
+    resources :organisms
+    resources :genomes
+    resources :experiments
+  end
+
   resources :features
-  resources :genomes
-  resources :experiments
   resources :references
   resources :tools
   match 'features/objects' => 'features#objects'
   match 'features/depth' => 'features#depth'
   match 'tools/genomic_sequence' => 'tools#genomic_sequence'
   match 'tools/export' => 'tools#export'
-  match 'begin' => 'organisms#index'
   match '/:controller(/:action(/:id))'
   match ':controller/:action.:format' => '#index'
 end
