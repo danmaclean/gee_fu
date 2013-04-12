1628e3223cc79680f96d3690f1bd57d093fe772e
Andy Shipman <andy@cllearview.com>
2013-04-12 17:14:32 +0100

    Versioning support for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ GeeFu::Application.configure do
   config.active_support.deprecation = :stderr
 
   config.action_mailer.default_url_options = { :host => 'localhost:9987' }
+
+  config.after_initialize do
+    PaperTrail.enabled = false
+  end
 end
