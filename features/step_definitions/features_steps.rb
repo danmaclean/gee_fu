076d2f556db7ff618f199b168b3e1dbe88734236
Andy Shipman <andy@cllearview.com>
2013-04-16 14:53:41 +0100

    Features for editing features!

--- features/step_definitions/features_steps.rb
+++ features/step_definitions/features_steps.rb
@@ -1,5 +1,15 @@
+When(/^I am ready to review features directly$/) do
+  visit "/features"
+end
+
 Given(/^I am reviewing features$/) do
   click_link "Features"
+end
+
+Given(/^I am reviewing a feature$/) do
+  click_link "Features"
+  fill_in "Database ID", with: Feature.first.id
+  click_button "Get feature"
 end
 
 When(/^I search by feature ID$/) do
@@ -14,6 +24,14 @@ Then(/^I should see the genome feature for that ID$/) do
   page.should have_content JSON.parse(Feature.first.group).join(' ')
 end
 
-When(/^I am ready to review features directly$/) do
-  visit "/features"
+When(/^I edit a feature and change the coordinates of the exon$/) do
+  click_link Feature.first.id
+  fill_in "Start Position", with: 1000
+  fill_in "End Position", with: 2000
+  click_button "Update Feature"
+end
+
+Then(/^there should be a new feature with changed coordinates$/) do
+  Feature.last.start.should eq 1000
+  Feature.last.end.should eq 2000
 end
