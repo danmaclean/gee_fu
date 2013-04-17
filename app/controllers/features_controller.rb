cbcd5aedd98be0ecdff32c93e2950305ab1bc2d5
Andy Shipman <andy@cllearview.com>
2013-04-17 14:34:47 +0100

    Be more specific about the search paths

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -2,9 +2,6 @@
 
 
 class FeaturesController < ApplicationController
-
-  before_filter :search, only: [ :index ]
-
   def index
     @genomes = Genome.all
     @experiments = Experiment.all
@@ -724,8 +721,22 @@ class FeaturesController < ApplicationController
     {:success => true }
   end
 
-  def search
-    redirect_to feature_path(params[:feature][:id]) if params.has_key?(:feature) && params[:feature].has_key?(:id)
+  def search_by_id
+    feature_id = params[:feature][:id]
+    if Feature.exists?(feature_id)
+      redirect_to feature_path(feature_id)
+    else
+      redirect_to features_path, flash: { alert: "No feature found with that ID"}
+    end
   end
- 
+
+  def search_by_attribute
+    attribute = params[:feature][:group]
+    @features = Feature.where{ group.matches("%#{attribute}%") }
+    if @features.empty?
+      redirect_to features_path, flash: { alert: "No features found searching for: '#{attribute}'"}
+    else
+      render
+    end
+  end
 end
