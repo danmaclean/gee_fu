e676f911ac049e1212cfdca5760757db8496cfd5
Andy Shipman <andy@cllearview.com>
2013-04-17 09:04:03 +0100

    Add PaperTrail to Genomes

--- app/models/genome.rb
+++ app/models/genome.rb
@@ -1,11 +1,13 @@
 class Genome < ActiveRecord::Base
   has_many :references, :dependent => :destroy
+
   validates_presence_of :build_version
   validates_presence_of :fasta_file
   
-  attr_accessor :fasta_file, :yaml_file
-
+  attr_accessor   :fasta_file, :yaml_file
   attr_accessible :build_version, :fasta_file, :yaml_file, :meta
+
+  has_paper_trail
   
   def html_meta
     self.meta ? self.meta.to_yaml.gsub!(/\n/,"<br/>").gsub!(/\s/,"&nbsp;").html_safe : ''
@@ -14,6 +16,4 @@ class Genome < ActiveRecord::Base
   def meta_as_data_structure
     self.meta ? JSON::parse(self.meta) : nil
   end
-  
-
 end
