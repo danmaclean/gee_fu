19a1c833a6baeae7f50faebca524a5920a2f1e2f
Andy Shipman <andy@cllearview.com>
2013-04-19 11:27:59 +0100

    Ensure a genome belongs to an organism

--- app/models/genome.rb
+++ app/models/genome.rb
@@ -1,11 +1,14 @@
 class Genome < ActiveRecord::Base
+  belongs_to :organism
   has_many :references, :dependent => :destroy
 
-  validates_presence_of :build_version
+  # validates_presence_of :build_version
   validates_presence_of :fasta_file
+
+  validates :build_version, presence: true, uniqueness: { scope: :organism_id, message: "Build version must be unique to a genome" }
   
   attr_accessor   :fasta_file, :yaml_file
-  attr_accessible :build_version, :fasta_file, :yaml_file, :meta
+  attr_accessible :build_version, :fasta_file, :yaml_file, :meta, :organism_id
 
   has_paper_trail
   
