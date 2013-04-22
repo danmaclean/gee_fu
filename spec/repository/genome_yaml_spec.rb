6b375420e2b8b8c4b9a00faac21af4855c735746
Andy Shipman <andy@cllearview.com>
2013-04-22 11:26:43 +0100

    Spec changes to add 'whodunnit'

--- spec/repository/genome_yaml_spec.rb
+++ spec/repository/genome_yaml_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 require 'genome_yaml'
 
 describe GenomeYaml do
+  let(:user) {  
+    user = User.new(
+      email:                  "fred@fred.com",
+      password:               "password",
+      password_confirmation:  "password",
+      first_name:             "Fred",
+      last_name:              "Bloggs",
+      admin:                   true
+    )
+    user.confirm!
+    user.save
+    user
+  }
+
   let(:organism) {  
     Organism.create!(
       :local_name => "My favourite organism",
@@ -14,25 +28,42 @@ describe GenomeYaml do
   }
 
   let(:genome) {  
-    Genome.create!(
-      organism_id: organism.id,
-      build_version: "TAIR 9",
-      meta: { }.to_json,
-      fasta_file: "dummy/not/actually/used/in/the/spec"
-    )
+    Timecop.freeze(Time.zone.local(2013, 4, 19, 12, 0, 0)) do
+      Genome.create!(
+        organism_id: organism.id,
+        build_version: "TAIR 9",
+        meta: { }.to_json,
+        fasta_file: "dummy/not/actually/used/in/the/spec"
+      )
+    end
   }
 
-  subject { GenomeYaml.new(genome) }
+  before(:each) do
+    PaperTrail.whodunnit = user.id
+  end
 
-  describe "#dump" do
-    let(:yaml) { YAML.load(subject.dump) }
+  subject    { GenomeYaml.new(genome) }
+  let(:yaml) { YAML.load(subject.dump) }
 
+  describe "#dump", versioning: true do
     it "writes a genome out as a YAML file" do
       yaml.should eq(
         "genome" => {
-          "Build version"  => "TAIR 9"#,
-          # "Created by"        => "Fred Bloggs",
-          # "Created on"        => "19 Apr 2013"
+          "Build version"   => "TAIR 9",
+          "Last updated by" => "Fred Bloggs",
+          "Last updated on" => "19 April 2013"
+        }
+      )
+    end
+  end
+
+  describe "#dump without versioning" do
+    it "should mark changes as Unknown" do
+      yaml.should eq(
+        "genome" => {
+          "Build version"   => "TAIR 9",
+          "Last updated by" => "Unknown",
+          "Last updated on" => "Unknown"
         }
       )
     end
