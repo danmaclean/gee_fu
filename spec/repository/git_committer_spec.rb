efb7e77c8283ce837080f7be3a801a874ca92333
Andy Shipman <andy@cllearview.com>
2013-04-24 16:26:26 +0100

    Only update git if we are in a separate repo

--- spec/repository/git_committer_spec.rb
+++ spec/repository/git_committer_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 describe GitCommitter do
+  include FakeFS::SpecHelpers
+
   let(:repo_path)       { GeeFu::Application.app_config[:repository_directory] }
   let(:full_repo_path)  { "#{Rails.root}/#{repo_path}" }
 
   subject { GitCommitter.new(full_repo_path) }
+
+  before(:each) do
+    FileUtils.mkdir_p(full_repo_path)
+  end
 
   describe "#commit" do
     it "changes to the data repo folder" do
@@ -12,11 +19,24 @@ describe GitCommitter do
       subject.commit
     end
 
-    it "calls `git add .`" do
-      subject.should_receive(:`).with("git add .").ordered
-      subject.should_receive(:`).with("git commit -am 'Update GeeFU data'").ordered
-      subject.should_receive(:`).with("git push origin master").ordered
-      subject.commit
+    context "there is a git repo in this folder" do
+      before(:each) do
+        FileUtils.mkdir_p("#{full_repo_path}/.git")
+      end
+
+      it "shells out to git" do
+        subject.should_receive(:`).with("git add .").ordered
+        subject.should_receive(:`).with("git commit -am 'Update GeeFU data'").ordered
+        subject.should_receive(:`).with("git push origin master").ordered
+        subject.commit
+      end
+    end
+
+    context "there is no git repo in this folder" do
+      it "doesn't shell out to git" do
+        subject.should_not_receive(:`)
+        subject.commit
+      end
     end
   end
 end
