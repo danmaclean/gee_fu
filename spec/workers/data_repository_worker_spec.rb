942b39e48149a51a58e8b5a7c2da44305859e1bc
Andy Shipman <andy@cllearview.com>
2013-04-24 10:39:50 +0100

    Update to use GitCommitter after the repo is created

--- spec/workers/data_repository_worker_spec.rb
+++ spec/workers/data_repository_worker_spec.rb
@@ -6,10 +6,18 @@ describe DataRepositoryWorker do
   let(:full_repo_path)  { "#{Rails.root}/#{repo_path}" }
 
   let(:root_repository) { mock(RootRepository, create: :specified_later) }
+  let(:git_committer)   { mock(GitCommitter, commit: :specified_later) }
 
-  it "uses a DataRepositoryCreator to do the work" do
-    RootRepository.should_receive(:new).with(full_repo_path).and_return(root_repository)
-    root_repository.should_receive(:create)
+  before(:each) do
+    RootRepository.stub(new: root_repository)
+    GitCommitter.stub(new: git_committer)
+  end
+
+  it "uses a RootRepository to create the data repository" do
+    RootRepository.should_receive(:new).with(full_repo_path).ordered
+    root_repository.should_receive(:create).ordered
+    GitCommitter.should_receive(:new).with(full_repo_path).ordered
+    git_committer.should_receive(:commit).ordered
     subject.perform
   end
 end
