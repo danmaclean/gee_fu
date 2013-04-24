30de573f37ed8d333e3359cf0a29bbdb9475c9f7
Andy Shipman <andy@cllearview.com>
2013-04-24 10:39:50 +0100

    Update to use GitCommitter after the repo is created

--- app/workers/data_repository_worker.rb
+++ app/workers/data_repository_worker.rb
@@ -1,9 +1,15 @@
 require 'root_repository'
+require 'git_committer'
 
 class DataRepositoryWorker
   include Sidekiq::Worker
 
   def perform
-    RootRepository.new("#{Rails.root}/#{GeeFu::Application.app_config[:repository_directory]}").create
+    RootRepository.new(repo_path).create
+    GitCommitter.new(repo_path).commit
+  end
+
+  def repo_path
+    "#{Rails.root}/#{GeeFu::Application.app_config[:repository_directory]}"
   end
 end
