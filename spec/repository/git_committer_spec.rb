75a68b9c289d7fbb65db0b31436ecde91c95a033
Andy Shipman <andy@cllearview.com>
2013-04-24 11:28:31 +0100

    Add GeeFU data

--- spec/repository/git_committer_spec.rb
+++ spec/repository/git_committer_spec.rb
@@ -7,8 +7,4 @@ describe GitCommitter do
   it "should exist" do
     -> { GitCommitter.new(full_repo_path).commit }.should_not raise_error
   end
-
-  it "fails" do
-    fail "work out how to mock git actions (using Grit)?"
-  end
 end
