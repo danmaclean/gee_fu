58913d0ea716523ec402bc8d7f4991aa7ee05172
Martin Page <wookoouk@gmail.com>
2013-07-12 11:14:04 +0100

    reorder

--- app/assets/javascripts/activelinks.js
+++ app/assets/javascripts/activelinks.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
 
 		 var url = window.location.pathname;  
 		 var activePage = url.substring(url.lastIndexOf('/'));
-		console.log(activePage);
+
+	     var pageLink = document.URL;
+	     console.log(activePage);
 	     $("#nav_menu").children("li").each(function(){
-	     console.log($(this).children("a:first-child").attr("href"));
+	    	 console.log($(this).children("a:first-child").attr("href"));
+
 	         if($(this).children("a:first-child").attr("href") == activePage){
 	             $(this).addClass("active");
 	         }
