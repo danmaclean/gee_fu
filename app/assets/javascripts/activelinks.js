593e27d9c4444fd0857b82e5a5b0349d0063baa9
Martin Page <wookoouk@gmail.com>
2013-07-12 10:27:50 +0100

    renamed div id selecter

--- app/assets/javascripts/activelinks.js
+++ app/assets/javascripts/activelinks.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	 $(function(){
 	     var pageLink = document.URL;
-	     $("#yourNavDiv").children("li").each(function(){
+	     $("#nav_menu").children("li").each(function(){
 	         if($(this).children("a:first-child").attr("href") == pageLink){
 	             $(this).addClass("active");
 	         }
