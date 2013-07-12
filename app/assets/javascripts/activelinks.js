fce06c52231869e8cfe54cac5d18055ee747da9b
Martin Page <wookoouk@gmail.com>
2013-07-12 10:30:05 +0100

    debug js

--- app/assets/javascripts/activelinks.js
+++ app/assets/javascripts/activelinks.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 	 $(function(){
 	     var pageLink = document.URL;
 	     $("#nav_menu").children("li").each(function(){
+	    	 console.log($(this).children("a:first-child").attr("href"))
+	    	 console.log(pageLink)
 	         if($(this).children("a:first-child").attr("href") == pageLink){
 	             $(this).addClass("active");
 	         }
