6a53f3882f96ad11ed73531aebaff87e2112aaaf
Martin Page <wookoouk@gmail.com>
2013-07-12 11:04:35 +0100

    js revert

---
+++ app/assets/javascripts/activelinks.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+	 $(function(){
+
+		 var url = window.location.pathname;  
+		 var activePage = url.substring(url.lastIndexOf('/'));
+
+	     $("#nav_menu").children("li").each(function(){
+	         if($(this).children("a:first-child").attr("href") == activePage){
+	             $(this).addClass("active");
+	         }
+	     });
+	  });
+  
+});
