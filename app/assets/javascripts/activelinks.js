d6336fc5fffc33a3caeffec619f82d380a2c1a5b
Martin Page <wookoouk@gmail.com>
2013-07-12 10:35:15 +0100

    remove debug output (working)

--- app/assets/javascripts/activelinks.js
+++ app/assets/javascripts/activelinks.js
@@ -5,11 +5,7 @@ $(document).ready(function () {
 		 var url = window.location.pathname;  
 		 var activePage = url.substring(url.lastIndexOf('/'));
 		 
-	     var pageLink = document.URL;
-	     console.log(activePage);
 	     $("#nav_menu").children("li").each(function(){
-	    	 console.log($(this).children("a:first-child").attr("href"));
-	    	 
 	         if($(this).children("a:first-child").attr("href") == activePage){
 	             $(this).addClass("active");
 	         }
