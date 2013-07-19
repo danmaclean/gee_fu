079a002596b83b87b86315404eec4f2bf51f62f0
Martin Page <wookoouk@gmail.com>
2013-07-19 10:05:34 +0100

    ss init moved to environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GeeFu::Application.initialize!
+SequenceServer::App.config_file = "config/sequenceserver.conf"
+SequenceServer::App.init
