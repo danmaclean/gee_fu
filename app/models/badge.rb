82aa039119d9374dd443a0d873046c159588677b
Martin Page <wookoouk@gmail.com>
2013-07-24 11:36:20 +0100

    replaced controller with model

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,8 +1,7 @@
-class BadgesController < ApplicationController
-
+class Badge
 def give_badge
 # gen badge
-   HTTParty.post("http://playbadges.tsl.ac.uk/api",
+  assert_url = HTTParty.post("http://playbadges.tsl.ac.uk/api",
     :headers => { "recipient" => current_user.email, "evidence" => "http://www.example.org", "badgeId" => "1"},
     :basic_auth => {:username => "martin.page@tsl.ac.uk", :password => "B0nF1rE"})
  # redirect to receive badge
