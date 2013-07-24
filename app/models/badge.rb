d3613944c53f9179c9fb3965e9b801ab62881e8d
Martin Page <wookoouk@gmail.com>
2013-07-24 13:53:33 +0100

    commented out badge code.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,9 +1,9 @@
-class Badge
-def give_badge
-# gen badge
-  assert_url = HTTParty.post("http://playbadges.tsl.ac.uk/api",
-    :headers => { "recipient" => current_user.email, "evidence" => "http://www.example.org", "badgeId" => "1"},
-    :basic_auth => {:username => "martin.page@tsl.ac.uk", :password => "B0nF1rE"})
- # redirect to receive badge
- # //TODO
-end
+# class Badge
+# def give_badge
+# # gen badge
+#   assert_url = HTTParty.post("http://playbadges.tsl.ac.uk/api",
+#     :headers => { "recipient" => current_user.email, "evidence" => "http://www.example.org", "badgeId" => "1"},
+#     :basic_auth => {:username => "martin.page@tsl.ac.uk", :password => "B0nF1rE"})
+#  # redirect to receive badge
+#  # //TODO
+# end
