08ee2485b3f4ac39eae1d4605d00d444ff1b8b7a
Martin Page <wookoouk@gmail.com>
2013-07-25 16:18:47 +0100

    fix for email comfirmation link;

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,6 @@ GeeFu::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'v0311.nbi.ac.uk' }
 
 end
