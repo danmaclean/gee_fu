95c11378ba4c18daa06b49a8cf6b901dd607918c
Martin Page <wookoouk@gmail.com>
2013-07-29 10:32:17 +0100

    test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -421,7 +421,7 @@ class FeaturesController < ApplicationController
   def dalliance_get
     # experiment = Feature.find(params[:id])
     # experiment = Feature.find(:reference_id => params[:id]) # UNKNOWN KEY :(
-    experiment = Feature.find(:all, :conditions => {:freference_id => params[:id]})
+    experiment = Feature.find(:all, :conditions => {:reference_id => params[:id]})
     render :xml => experiment,  :layout => false
   end
 
