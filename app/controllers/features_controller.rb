71cd35aaaead1271b5a719c2f41cdad424b8e006
Martin Page <wookoouk@gmail.com>
2013-07-30 12:26:16 +0100

    test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -421,7 +421,7 @@ class FeaturesController < ApplicationController
   def dalliance_get
     # experiment = Feature.find(params[:id])
     # @experiment = Experiment.find(params[:id]).features
-    experiment = Feature.find(:all, :conditions => {:experiment.id => params[:id]})
+    experiment = Feature.find(:all, :conditions => {:experiment => Experiment.find(params[:id])})
     render :xml => @experiment,  :layout => false
   end
 
