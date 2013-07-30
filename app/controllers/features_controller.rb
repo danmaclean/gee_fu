3b9e5b88c24968575944a767633b7ee18b8aac87
Martin Page <wookoouk@gmail.com>
2013-07-30 10:26:35 +0100

    test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -133,8 +133,8 @@ class FeaturesController < ApplicationController
 
   def show 
     begin
-      today = `date`
-      puts "#{today}"
+      today = `echo helloworld`
+      logger.debug "New post: #{today}"
       @feature = Feature.find(params[:id])
       respond @feature
     rescue
