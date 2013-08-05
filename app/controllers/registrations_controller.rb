aec8d18c0c82f8607899c721974ba6bdd72e1568
Martin Page <wookoouk@gmail.com>
2013-08-05 16:56:54 +0100

    add new user test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     signed_up_path
   end
+
+  def create
+    logger.debug "DEBUG - CREATE WebApollo user here!"
+  end
 end
