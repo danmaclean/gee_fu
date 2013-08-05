84393a28668b64f5f6823b88b20cee02d50c6f69
Martin Page <wookoouk@gmail.com>
2013-08-05 16:51:35 +0100

    Add bam track in WebApollo with GeeFu file

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -143,6 +143,15 @@ class ExperimentsController < ApplicationController
       end
     elsif @experiment.expected_file == "bam"
       @experiment.uses_bam_file = true
+      logger.debug "DEBUG: Going to pass #{@experiment.bam_file_path} to WebApollo as a BAM"
+
+      cmdOne = system('#{WebApolloAppPath}/jbrowse/bin/add_bam_track.pl --bam_url #{@experiment.bam_file_path} --label simulated_bam --key "simulated BAM"')
+
+      cmdComplete = "SUCCESSFUL"
+      if(!cmdOne)
+        cmdComplete = "FAILED, Please add manually"
+      end
+      logger.debug "cmdOne #{cmdOne}"
     end
     
     if @experiment.save
