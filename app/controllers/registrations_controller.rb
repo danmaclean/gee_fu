030538b915c5b41a00368035c1493c649b1912ed
Martin Page <wookoouk@gmail.com>
2013-08-06 16:35:13 +0100

    add ability to set permissions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,11 +4,18 @@ class RegistrationsController < Devise::RegistrationsController
 	def create
 		email = params[:user][:email]
 		password = params[:user][:password]
+
 		logger.error "Hello #{email} #{password}"
+
 		webApolloOutput = `createuser -U postgres #{email} --createdb --no-superuser --no-createrole`
 		logger.error "create user output: #{webApolloOutput}"
+
 		webApolloOutputTwo = `#{WebApolloPath}/tools/user/add_user.pl -D #{WebApolloDatabase} -U #{WebApolloDatabaseUsername} -P #{WebApolloDatabasePassword} -u #{email} -p #{password}`
 		logger.error "add user to apollo output: #{webApolloOutputTwo}"
+		
+		webApolloOutputThree = `sudo -u postgres #{WebApolloPath}/tools/user/set_track_permissions.pl -D #{WebApolloDatabase}   –U #{WebApolloDatabaseUsername} -P #{WebApolloDatabasePassword} -a -u #{email} -t /opt/WebApollo/data/scratch/seqids.txt`
+		logger.error "set apollo user permissions output: #{webApolloOutputThree}"
+
 		# if(waUserAdded)
 		# 	logger.error "{email} user added to database"
 		# else
