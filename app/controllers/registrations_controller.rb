c7eda917152f6418da707de4ee36059e7cde5287
Martin Page <wookoouk@gmail.com>
2013-08-06 09:38:35 +0100

    test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
+	def new
+		logger.debug "DEBUG - HELLO WORLD"
+      super
+    end
+
   def after_inactive_sign_up_path_for(resource)
     signed_up_path
   end
