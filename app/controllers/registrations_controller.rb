46aa3444051725a83551a40a1e2f9417c2611aba
Martin Page <wookoouk@gmail.com>
2013-08-06 10:01:14 +0100

    test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   
 
-  def create
-	logger.debug "DEBUG - Hello"
-  end
+	def create
+		logger.debug "DEBUG - Hello"
+  	end
 
-  def after_inactive_sign_up_path_for(resource)
-    signed_up_path
-  end
+	protected
+
+  	def after_inactive_sign_up_path_for(resource)
+    	signed_up_path
+  	end
 
 end
