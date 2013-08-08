a23cb93b173e2e962bec6866a8e7cee3b766f253
Martin Page <wookoouk@gmail.com>
2013-08-08 13:40:02 +0100

    add back in bam support; gff imports running smooth

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -141,7 +141,7 @@ class ExperimentsController < ApplicationController
       @experiment.uses_bam_file = true
       logger.debug "DEBUG: Going to pass #{@experiment.bam_file_path} to WebApollo as a BAM"
 
-#      cmdOne = system('#{WebApolloAppPath}/jbrowse/bin/add_bam_track.pl --bam_url #{@experiment.bam_file_path} --label simulated_bam --key "simulated BAM"')
+      cmdOne = system('#{WebApolloAppPath}/jbrowse/bin/add_bam_track.pl --bam_url #{@experiment.bam_file_path} --label simulated_bam --key "simulated BAM"')
 
 #      cmdComplete = "SUCCESSFUL"
 #      if(!cmdOne)
