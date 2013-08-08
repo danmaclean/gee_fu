435ba0c1a6a9e2fe9e623adceb55d5ac6625bc88
Martin Page <wookoouk@gmail.com>
2013-08-08 14:02:13 +0100

    full path output

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -141,7 +141,7 @@ class ExperimentsController < ApplicationController
       @experiment.uses_bam_file = true
       logger.debug "DEBUG: Going to pass #{@experiment.bam_file_path} to WebApollo as a BAM"
 
-      cmdOne = `#{WebApolloAppPath}/jbrowse/bin/add_bam_track.pl --bam_url #{@experiment.bam_file_path} --label simulated_bam --key "simulated BAM"`
+      cmdOne = `#{WebApolloAppPath}/jbrowse/bin/add_bam_track.pl --bam_url #{@experiment.bam_file_path} --label simulated_bam --key "simulated BAM" --out #{WebApolloAppPath}/jbrowse/data/trackList.json`
 
 #      cmdComplete = "SUCCESSFUL"
 #      if(!cmdOne)
