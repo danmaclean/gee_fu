26a70cb994441e23c8a7e8d2c6d8fafcbf6bc327
Martin Page <wookoouk@gmail.com>
2013-08-08 13:23:23 +0100

    test

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -50,7 +50,7 @@ class ExperimentsController < ApplicationController
 
       logger.debug "DEBUG: Going to pass #{@experiment.gff_file.path} to WebApollo as a GFF"
 
-      filenamebase = @experiment.gff_file.name
+      filenamebase = @experiment.name
       
       cmdOne = `#{WebApolloAppPath}/jbrowse/bin/flatfile-to-json.pl --gff #{@experiment.gff_file.path} --arrowheadClass webapollo-arrowhead --getSubfeatures --trackLabel #{filenamebase} --webApollo --renderClassName gray-center-20pct --out #{WebApolloAppPath}/jbrowse/data/`
       
