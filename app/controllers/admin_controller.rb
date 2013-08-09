e86d89437c0a541d8d36f5606a500483f8359181
Martin Page <wookoouk@gmail.com>
2013-08-09 16:23:16 +0100

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,7 +12,7 @@ class AdminController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user][:id])
+    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # Handle a successful update.
       render 'index'
