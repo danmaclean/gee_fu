7fb1a70eed9c8a29844a1b5b2ecf05c7ec30c752
Martin Page <wookoouk@gmail.com>
2013-08-09 15:33:21 +0100

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,19 @@ class AdminController < ApplicationController
   end
 
   def show
-      @user = User.find(params[:id])
-    end
-  
+    @user = User.find(params[:id])
+  end
+
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 end
