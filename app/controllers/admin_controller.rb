a5ffb7f612ed27c776afc241f8620635191881eb
Martin Page <wookoouk@gmail.com>
2013-08-09 16:36:08 +0100

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,7 +16,7 @@ class AdminController < ApplicationController
     if @user.update_attributes(params[:user])
       # Handle a successful update.
       @user.save
-      redirect_to index
+      redirect_to action: 'index'
       #    else
       #      render 'edit'
     end
