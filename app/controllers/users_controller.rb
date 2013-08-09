8935b5e478a030254696ad209c17ea09bd1a0e15
Martin Page <wookoouk@gmail.com>
2013-08-09 13:13:02 +0100

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :get_user, :only => [:index,:new,:edit]
   before_filter :accessible_roles, :only => [:new, :edit, :show, :update, :create]
-  load_and_authorize_resource :only => [:show,:new,:destroy,:edit,:update]
+#  load_and_authorize_resource :only => [:show,:new,:destroy,:edit,:update]
  
   # GET /users
   # GET /users.xml                                                
