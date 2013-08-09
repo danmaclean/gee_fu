bee1e41c8498405eb07c1fa921fdaadec9bc7b7e
Martin Page <wookoouk@gmail.com>
2013-08-09 16:14:58 +0100

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,7 +15,7 @@ class AdminController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # Handle a successful update.
-      puts "id = #{params[:id]}"
+      logger.error "id = #{params[:id]}"
       render 'index'
     else
       render 'edit'
