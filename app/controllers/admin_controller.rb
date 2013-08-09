8b29501af06a4448f3bbc2bd8030cfe93d5b0b38
Martin Page <wookoouk@gmail.com>
2013-08-09 15:26:52 +0100

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,10 @@ class AdminController < ApplicationController
     @users = User.all
   end
 
+  def show
+      @user = User.find(params[:id])
+    end
+  
   def edit
     @user = User.find(params[:id])
   end
