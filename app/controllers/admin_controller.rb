6a4f78e6a25828c31c4990ab111c9e9c97e91ea3
Martin Page <wookoouk@gmail.com>
2013-08-09 15:49:34 +0100

    rwar

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,7 +15,7 @@ class AdminController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # Handle a successful update.
-      redirect_to @user
+      redirect_to index
     else
       render 'edit'
     end
