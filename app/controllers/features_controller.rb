1798d7785547fee1a7a59e5387367629a878363e
Martin Page <wookoouk@gmail.com>
2013-10-02 14:08:16 +0100

    Get JUST mrna values

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -430,8 +430,8 @@ class FeaturesController < ApplicationController
 
     featureType = params[:datatype]
       if featureType.nil? then
-        # @types = Feature.where(experiment_id: params[:exid]).limit(featurelimit).pluck(:feature)
-        @experiment = Feature.where(experiment_id: params[:exid]).limit(featurelimit)
+        # @experiment = Feature.where(experiment_id: params[:exid]).limit(featurelimit)
+         @experiment = Feature.where(experiment_id: params[:exid], feature: ["five_prime_UTR", "exon", "intron","three_prime_UTR"]).limit(featurelimit)
       else
         @experiment = Feature.where(experiment_id: params[:exid], feature: featureType).limit(featurelimit)
       end
