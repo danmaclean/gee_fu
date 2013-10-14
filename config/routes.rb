61d58511f46d8f66972efa28c7dfefdb44ef2e52
Martin Page <wookoouk@gmail.com>
2013-10-14 16:00:44 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ GeeFu::Application.routes.draw do
   scope "/features/dalliance" do
   	get "/:exid", to: "features#dalliance_get"
     get "/reference/:exid", to:"features#dallience_reference"
-    get "/reference/:exid.xml/sequence", to:"features#dallience_reference"
+    get "/reference/:exid'.xml'/sequence", to:"features#dallience_reference"
   end
 
   scope "/genomes/annoj" do
