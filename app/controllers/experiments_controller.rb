12ebd8553262b5bdb16a474d016131278a5cbab3
Martin Page <wookoouk@gmail.com>
2013-10-21 14:23:41 +0100

    test

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -60,13 +60,14 @@ class ExperimentsController < ApplicationController
 
       filenamebase = @experiment.name.downcase.tr(" ", "_")
       
-      cmdOne = `#{WebApolloAppPath}/jbrowse/bin/flatfile-to-json.pl --gff #{@experiment.gff_file.path} --getSubfeatures --trackLabel #{filenamebase} --webApollo --out #{WebApolloAppPath}/jbrowse/data/`
+      cmdOne = '#{WebApolloAppPath}/jbrowse/bin/flatfile-to-json.pl --gff #{@experiment.gff_file.path} --getSubfeatures --trackLabel #{filenamebase} --webApollo --out #{WebApolloAppPath}/jbrowse/data/'
       
-#     cmdOne = `#{WebApolloAppPath}/jbrowse/bin/flatfile-to-json.pl --gff #{@experiment.gff_file.path} --arrowheadClass webapollo-arrowhead --getSubfeatures --trackLabel #{filenamebase} --webApollo --renderClassName gray-center-20pct --out #{WebApolloAppPath}/jbrowse/data/`
+#     cmdOne = '#{WebApolloAppPath}/jbrowse/bin/flatfile-to-json.pl --gff #{@experiment.gff_file.path} --arrowheadClass webapollo-arrowhead --getSubfeatures --trackLabel #{filenamebase} --webApollo --renderClassName gray-center-20pct --out #{WebApolloAppPath}/jbrowse/data/''
+
 
 #     bin/flatfile-to-json.pl --gff $WEB_APOLLO_SAMPLE_DIR/split_gff/maker.gff --getSubfeatures --type mRNA --trackLabel maker
 
-      cmdTwo = `#{WebApolloAppPath}/jbrowse/bin/generate-names.pl --out #{WebApolloAppPath}/jbrowse/data`
+      cmdTwo = '#{WebApolloAppPath}/jbrowse/bin/generate-names.pl --out #{WebApolloAppPath}/jbrowse/data'
 
 
 #      cmdComplete = "SUCCESSFUL"
@@ -76,6 +77,9 @@ class ExperimentsController < ApplicationController
 
       logger.error "cmdOne = #{cmdOne}"
       logger.error "cmdTwo = #{cmdTwo}"
+
+      `#{cmdOne}`
+      `#{cmdTwo}`
 
 
       File.open( "#{@experiment.gff_file.path}" ).each do |line|
