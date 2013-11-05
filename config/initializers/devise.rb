84659908f226cce597a75ef60196d6c9da6a211a
Martin Page <wookoouk@gmail.com>
2013-11-05 16:14:13 +0000

    hardcode

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,10 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  require 'dotenv'
+  Dotenv.load
+
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
