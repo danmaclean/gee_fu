d9e8e541510f43fcd415a09d0623d11da87bd44a
Martin Page <wookoouk@gmail.com>
2013-11-05 13:23:33 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,5 +42,9 @@ GeeFu::Application.configure do
       :protocol => 'https',
       :only_path => false,
   }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.raise_delivery_errors = true
 end
