a83c88118b870729558ddf7b5f5500072916050c
Martin Page <wookoouk@gmail.com>
2013-11-05 13:19:43 +0000

    test

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -6,5 +6,12 @@
       :password =>       ENV['MANDRILL_APIKEY'],
       :authentication => :plain
   }
+
+
   ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 #end
