d4e0c6ed0615cba3dbb21d73184578ad6d034e02
Martin Page <wookoouk@gmail.com>
2013-11-05 13:15:25 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,16 +42,5 @@ GeeFu::Application.configure do
       :protocol => 'https',
       :only_path => false,
   }
-
-  #if %w(development production).include?(Rails.env)
-    ActionMailer::Base.smtp_settings = {
-        :port =>           '587',
-        :address =>        'smtp.mandrillapp.com',
-        :user_name =>      ENV['MANDRILL_USERNAME'],
-        :password =>       ENV['MANDRILL_APIKEY'],
-        :authentication => :plain
-    }
-    ActionMailer::Base.delivery_method = :smtp
-  #end
-
+  config.action_mailer.raise_delivery_errors = true
 end
