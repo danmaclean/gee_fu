7d1e2a73008d6fc74c76b9f30d44d535c307a84b
Martin Page <wookoouk@gmail.com>
2013-11-08 16:45:05 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,81 +1,82 @@
 GeeFu::Application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   root :to => "pages#index"
-  
-  get "/browse",     :to => "pages#browse" 
-  get "/signed_up",  :to => "pages#signed_up", :as => :signed_up
+
+  get "/browse", :to => "pages#browse"
+  get "/signed_up", :to => "pages#signed_up", :as => :signed_up
   get 'begin' => 'pages#index'
 
   #authenticate :user do
   #  resources :organisms
   #  resources :genomes
-    resources :experiments
+  #  resources :experiments
   #  resources :features
   #  resources :tools
   #  resources :admin
 
-    scope "/features/search" do
-      post "/id",        to: "features#search_by_id",         as: :feature_search_by_id
-      post "/attribute", to: "features#search_by_attribute",  as: :feature_search_by_attribute
-      post "/summary",   to: "features#summary",              as: :feature_search_by_range
-    end
-
-    scope "/tools" do
-      post "/sequence",  to: "tools#genomic_sequence",        as: :tools_genomic_sequence
-    end
-
-    scope "/genomes" do
-      get "/reference_list", to: "genomes#reference_list"
-    end
-
-    scope "/experiments" do
-      get "/reference_list", to: "experiments#reference_list"
-    end
+  scope "/features/search" do
+    post "/id", to: "features#search_by_id", as: :feature_search_by_id
+    post "/attribute", to: "features#search_by_attribute", as: :feature_search_by_attribute
+    post "/summary", to: "features#summary", as: :feature_search_by_range
   end
 
-  scope "/features/annoj" do
-    get  "/:id", to: "features#annoj_get"
-    post "/:id", to: "features#annoj_post"
+  scope "/tools" do
+    post "/sequence", to: "tools#genomic_sequence", as: :tools_genomic_sequence
   end
 
-  scope "/features/dalliance" do
-    get "/part/:exid/:part", to: "features#dalliance_part"
-  	get "/:exid/:part", to: "features#dalliance_get"
-    get "/:exid/:part/:featutype/", to: "features#dalliance_get"
+  scope "/genomes" do
+    get "/reference_list", to: "genomes#reference_list"
   end
 
-  scope "/genomes/dalliance/" do
-    #get "/:exid/:part", to: "features#dalliance_genome"
-    get "/:exid/:part/sequence", to: "features#dalliance_genome"
+  scope "/experiments" do
+    get "/reference_list", to: "experiments#reference_list"
   end
+end
+
+scope "/features/annoj" do
+  get "/:id", to: "features#annoj_get"
+  post "/:id", to: "features#annoj_post"
+end
+
+scope "/features/dalliance" do
+  get "/part/:exid/:part", to: "features#dalliance_part"
+  get "/:exid/:part", to: "features#dalliance_get"
+  get "/:exid/:part/:featutype/", to: "features#dalliance_get"
+end
+
+scope "/genomes/dalliance/" do
+  #get "/:exid/:part", to: "features#dalliance_genome"
+  get "/:exid/:part/sequence", to: "features#dalliance_genome"
+end
 
 scope "/users" do
   get "/admin", to: "AdminController#index"
 end
 
-  scope "/genomes/annoj" do
-    get  "/:id", to: "genomes#annoj"
-  end
+scope "/genomes/annoj" do
+  get "/:id", to: "genomes#annoj"
+end
 
-  match '/admin/:id', :to => 'admin#show', :as => :user
+match '/admin/:id', :to => 'admin#show', :as => :user
 
- mount SequenceServer::App, :at => "sequenceserver"
+mount SequenceServer::App, :at => "sequenceserver"
 
- 
- scope "/sequenceserver" do 
+
+scope "/sequenceserver" do
   get "/get_sequence", to: "experiments#findfromss"
 end
 
 match "/webapollo" => redirect("http://geefu.oadb.tsl.ac.uk:8080/WebApollo/"), :as => :webapollo
 
 
-  # TODO review these for security
-  resources :references
-  match 'features/objects' => 'features#objects'
-  match 'features/depth' => 'features#depth'
-  match 'tools/genomic_sequence' => 'tools#genomic_sequence'
-  match 'tools/export' => 'tools#export'
-  match '/:controller(/:action(/:id))'
-  # match ':controller/:action.:format' => '#index'
-end
+# TODO review these for security
+resources :references
+resources :experiments
+match 'features/objects' => 'features#objects'
+match 'features/depth' => 'features#depth'
+match 'tools/genomic_sequence' => 'tools#genomic_sequence'
+match 'tools/export' => 'tools#export'
+match '/:controller(/:action(/:id))'
+# match ':controller/:action.:format' => '#index'
+#end
