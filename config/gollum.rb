d94cbe91feca22a7508a8446fe440ea25daaae75
Martin Page <wookoouk@gmail.com>
2013-11-11 11:54:41 +0000

    gollum test

--- config/gollum.rb
+++ config/gollum.rb
@@ -5,4 +5,5 @@ gollum_path = File.expand_path(File.dirname("/opt/wiki")) # CHANGE THIS TO POINT
 Precious::App.set(:gollum_path, gollum_path)
 Precious::App.set(:default_markup, :markdown) # set your favorite markup language
 Precious::App.set(:wiki_options, {:universal_toc => false})
+Precious::App.set(:port, 81)
 run Precious::App
