58f05a2dafdfcbc923aea45ee822d768007e628b
Martin Page <wookoouk@gmail.com>
2013-11-12 10:54:00 +0000

    test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -236,7 +236,7 @@ class FeaturesController < ApplicationController
     flash[:error] << 'A reference sequence name must be provided' if reference.length == 0
     flash[:error] << 'Start must be a positive numeric value' if start =~ /\D/
     flash[:error] << 'End must be a positive numeric value' if _end =~ /\D/
-    flash[:error] << "End value must be greater than (or equal to) start value #{start} #{_end}" if _end < start
+    flash[:error] << "End value must be greater than (or equal to) start value #{start} #{_end}" if _end.to_i < start.to_i
 
     redirect_to(:back) and return unless flash[:error].empty?
 
