efbe26a3f79406db45c1f542bf63c1ce06619afa
Martin Page <wookoouk@gmail.com>
2013-11-13 11:58:33 +0000

    test

--- app/assets/javascripts/activelinks.js
+++ app/assets/javascripts/activelinks.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
 	 $(function(){
 
 		 var url = window.location.pathname;
-		 var activePage = url.substring(url.lastIndexOf('/'));
-//         alert(activePage);
+//		 var activePage = url.substring(url.lastIndexOf('/'));
+         var activePage = url.substring(url.indexOf('/'), 2);
+
+         alert("DEBUG: "+activePage);
 
 	     $("#nav_menu").children("li").each(function(){
 	         if($(this).children("a:first-child").attr("href") == activePage){
