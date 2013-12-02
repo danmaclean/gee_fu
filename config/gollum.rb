6445f118caa03f3b941bd23e8929573baa2391ce
Martin Page <wookoouk@gmail.com>
2013-12-02 15:52:54 +0000

    test

--- config/gollum.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'gollum/app'
-
-gollum_path = File.expand_path(File.dirname("/opt/wiki")) # CHANGE THIS TO POINT TO YOUR OWN WIKI REPO
-Precious::App.set(:gollum_path, gollum_path)
-Precious::App.set(:default_markup, :markdown) # set your favorite markup language
-Precious::App.set(:wiki_options, {:universal_toc => false})
-Precious::App.set(:port, "8081")
-run Precious::App
