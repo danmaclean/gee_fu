54326375e63c68cb021f19c57c16bf6fcb62ffdb
Martin Page <wookoouk@gmail.com>
2013-12-10 09:38:40 +0000

    typeahead test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -779,6 +779,10 @@ class FeaturesController < ApplicationController
 
 
   def genome_typeahead
+    query = params[:query]
 
+     @reference = Reference.where(["name LIKE :tag", {:tag => query}]) 
+
+     render json: @reference
   end
 end
