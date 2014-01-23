34d04c9185509201c55c332662d86c2ca4167182
Martin Page <martin.page@tsl.ac.uk>
2014-01-23 16:23:12 +0000

    compression

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,8 @@ GeeFu::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  config.assets.compress = true
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
