6d4a11d2c9a8745137e72d1a8e819db7f42348e3
Martin Page <martin.page@tsl.ac.uk>
2014-02-03 13:22:06 +0000

    test

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -728,7 +728,7 @@ badge = 6
     genome = Genome.find(params[:genome_build])
     reference = Reference.where(name: params[:build_name])
 
-if Reference.exists(reference) && Genome.exists(genome)
+if Reference.exists?(reference) && Genome.exists?(genome)
 
 reference_id = reference.id
 # get the name of the genome
